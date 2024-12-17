fad8b52f06c4193c90b64b2042d3c6b2fec77117
Kzuu1023 <delafiora552@gmail.com>
2024-12-16 20:10:49 -0800

    feat: displaying error

---
+++ contact-form-main/script.js
@@ -0,0 +1,17 @@
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const genInquiry = document.getElementById("inquiry");
+const reqSupport = document.getElementById("request");
+const message = document.getElementById("message");
+const consent = document.getElementById("consent");
+const submitBtn = document.getElementById("submit-btn");
+const form = document.getElementById("form");
+
+const inputs = [fname, lname, email, genInquiry, reqSupport, message, consent];
+
+let isValid = true;
+
+submitBtn.addEventListener("click", function () {
+    inputs.forEach((input) => {});
+});
